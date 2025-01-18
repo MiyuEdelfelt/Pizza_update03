ec6dee02ab85dd4689c0f6b85ceeb203a0716e25
MiyuEdelfelt <guillermopgr@gmail.com>
2025-01-18 00:40:01 -0300

    Hito 4, se deja habilitado el Home y comentado el Pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import Cart from "./components/Cart";
 import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
-import Pizza from "./components/Pizza";
-//import Home from "./components/Home";
+//import Pizza from "./components/Pizza";
+import Home from "./components/Home";
 // import Login from "./components/Login";
 // import Register from "./components/Register";
 
@@ -12,8 +12,8 @@ function App() {
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
       <main className="flex-grow-1">
-        <Pizza />
-        {/* <Home /> */}
+        {/* <Pizza /> */}
+        <Home />
         {/* <Register /> */}
         {/* <Login /> */}
         {/* <Cart /> */}
